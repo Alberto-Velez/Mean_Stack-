d30cbaec7e5968d71c80c0c2ec311e1a2ceaa118
Alberto Velez <nkkvlz@gmail.com>
2016-06-06 19:12:04 -0400

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,77 @@
+
+import {DemoService} from './demo.service';
+import {Component} from 'angular2/core';
+import {FORM_DIRECTIVES} from "angular2/common";
+import {HTTP_PROVIDERS} from 'angular2/http';
+
+
+@Component({
+  selector: 'demo-app',
+  template:`
+  <h1>Angular2 HTTP Demo App</h1>
+
+  <form f="postForm" (ngSubmit)="doPost()">
+      <button type="submit" class="btn btn-warning btn-lg">POST</button>
+      <input [(ngModel)]="call_post" placeholder="0">
+  </form>
+
+  <h2>Foods</h2>
+  <ul>
+    <li *ngFor="#food of foods">{{food.name}}</li>
+  </ul>
+  <h2>Books and Movies</h2>
+  <h3>Books</h3>
+  <ul>
+    <li *ngFor="#book of books">{{book.title}}</li>
+  </ul>
+  <h3>Movies</h3>
+  <ul>
+    <li *ngFor="#movie of movies">{{movie.title}}</li>
+  </ul>
+  `,
+
+})
+export class AppComponent {
+
+
+  public foods;
+  public books;
+  public movies;
+  public call_post;
+
+  constructor(private _demoService: DemoService) { }
+
+  ngOnInit() {
+    this.getFoods();
+    this.getBooksAndMovies();
+
+  }
+
+  doPost() {
+
+    this._demoService.post(this.call_post);
+
+   }
+
+  getFoods() {
+    this._demoService.getFoods().subscribe(
+      // the first argument is a function which runs on success
+      data => { this.foods = data},
+      // the second argument is a function which runs on error
+      err => console.error(err),
+      // the third argument is a function which runs on completion
+      () => console.log('done loading foods')
+    );
+  }
+
+  getBooksAndMovies() {
+    this._demoService.getBooksAndMovies().subscribe(
+      data => {
+        this.books = data[0]
+        this.movies = data[1]
+      }
+      // No error or completion callbacks here. They are optional, but
+      // you will get console errors if the Observable is in an error state.
+    );
+  }
+}
---
+++ server.js
@@ -0,0 +1,75 @@
+var express = require('express'),
+app= express(),
+engines = require('consolidate'),
+MongoClient = require('mongodb').MongoClient,
+assert = require('assert');
+bodyParser = require('body-parser');
+
+
+
+var app = express();
+
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// view engine setup
+
+
+
+
+
+// catch 404 and forward to error handler
+
+MongoClient.connect('mongodb://localhost:27017/munchies', function(err, db){
+
+  assert.equal(null, err);
+      console.log("Successfully connected to MongoDB.");
+
+
+app.get('/', function(req, res){
+res.render('index.html' );
+
+});
+
+app.get('/name', function(req, res){
+db.collection('names').find().toArray(function(err, docs) {
+            if (!err) {
+
+                console.log(docs);
+                res.json(docs);
+
+
+}
+else{
+  console.log(err);
+}
+})
+});
+
+
+   app.post('/calls', function(req, res) {
+       console.log("Post body.value is "+ req.body.value)
+       response={
+         "name": req.body.value
+       }
+db.collection("names").insert(response, {w:1}, function(err, result) {});
+
+   });
+
+
+app.use(function(req, res){
+  res.sendStatus(404);
+});
+
+
+var server = app.listen(3000, function() {
+  var port=server.address().port;
+  console.log('express server listening on port %s', port);
+});
+
+});
+
+module.exports = app;
